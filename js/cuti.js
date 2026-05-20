67f1f94ef729107565836e16d132e1c007a87fb2
ardymhmmad <ardy.quino@gmail.com>
2026-05-20 22:59:29 +0800

    Update cuti.js

--- js/cuti.js
+++ js/cuti.js
@@ -963,7 +963,7 @@ function cetakSuratCuti(id){
         dengan ketentuan sebagai berikut :
       </p>
 
-      <table style="width:120%;border-collapse:collapse;border:none;margin-left:24px;margin-bottom:8px">
+      <table style="width:100%;border-collapse:collapse;border:none;margin-left:24px;margin-bottom:8px">
         <tr style="border:none">
           <td style="width:30px;vertical-align:top;padding:2px 0;border:none;font-size:12pt;color:#000">a.</td>
           <td style="padding:2px 0;text-align:justify;border:none;font-size:12pt;color:#000;line-height:1.6">Sebelum menjalankan ${jenisCuti} ${tahun} wajib menyerahkan pekerjaannya kepada Atasan Langsung atau pejabat yang ditentukan.</td>
