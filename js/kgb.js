a0a70a99fb7d4cdd558f77f5c15a6ddacbc1e7e1
ardymhmmad <ardy.quino@gmail.com>
2026-05-24 00:00:49 +0800

    Update kgb.js

--- js/kgb.js
+++ js/kgb.js
@@ -371,7 +371,7 @@ function eksekusiCetakSKKGB(id){
 
     <p style="margin-bottom:10pt;line-height:1.5;text-indent:30px">Diberikan kenaikan gaji berkala hingga memperoleh :</p>
 
-    <table style="width:100%;border-collapse:collapse;border:none;margin-bottom:16pt">
+    <table style="width:100%;border-collapse:collapse;border:none;margin-bottom:10pt">
       <tr>
         <td style="width:22px;vertical-align:top;border:none;padding:2px 0">6.</td>
         <td style="width:190px;vertical-align:top;border:none;padding:2px 0">Gaji Pokok Baru</td>
@@ -404,7 +404,7 @@ function eksekusiCetakSKKGB(id){
       </tr>
     </table>
 
-    <p style="text-align:justify;margin-bottom:3pt;line-height:1;text-indent:30px">
+    <p style="text-align:justify;margin-bottom:3pt;line-height:1,5;text-indent:30px">
       Diharapkan agar sesuai dengan Peraturan Pemerintah yang berlaku dan Keputusan Presiden berikutnya, maka sesuai Anggaran Pendapatan dan Belanja Daerah tahun yang bersangkutan, kepada pegawai tersebut dapat dibayarkan penghasilannya berdasarkan gaji pokoknya yang baru.
     </p>
 
